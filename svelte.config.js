76b57f5360b071be87799f6b0dbbc206eba71c2f
Cole Boggus <cboggus@gmail.com>
2023-05-01 01:33:29 -0500

    add workflow

--- svelte.config.js
+++ svelte.config.js
@@ -8,25 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			paths: {
-				base: process.env.NODE_ENV === 'production' ? '/coleboggus.github.io' : ''
-			}
-		}),
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to shiny 404 page
-				if (path === '/not-found') {
-					return;
-				}
-
-				// otherwise fail the build
-				throw new Error(message);
-			}
-		}
+		adapter: adapter()
 	}
 };
 
