d24c11de28e67c42324168ae0b2de0596edebb02
Cole Boggus <cboggus@gmail.com>
2023-05-01 01:20:10 -0500

    Sveltekit basis established

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-github';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,13 +12,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'docs',
-			assets: 'docs',
-			precompress: false,
-			domain: '',
-			jekyll: false
+			paths: {
+				base: process.env.NODE_ENV === 'production' ? '/coleboggus.github.io' : ''
+			}
 		}),
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
